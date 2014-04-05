f91fd6651f5d5e464c759677b02fb021c3db8dd4
Prescott Murphy <prurph@gmail.com>
2014-04-05 09:36:12 -0400

    Patient index view as Bootstrapped table

--- source/care_dashboard/app/models/patient.rb
+++ source/care_dashboard/app/models/patient.rb
@@ -1,2 +1,13 @@
 class Patient < ActiveRecord::Base
+  def name_last_comma_first
+    "#{last_name}, #{first_name}"
+  end
+
+  def print_progress
+    "#{program_progress}/#{program_length}"
+  end
+
+  def print_today_completion
+    "#{task_completion}/#{task_count}"
+  end
 end
