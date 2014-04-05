a3cf21f587f9d37c4a23acea4f85d7b94f50bf37
Prescott Murphy <prurph@gmail.com>
2014-04-05 10:05:16 -0400

    print_compliance method to convert compliance boolean to string for display

--- source/care_dashboard/app/models/patient.rb
+++ source/care_dashboard/app/models/patient.rb
@@ -3,6 +3,15 @@ class Patient < ActiveRecord::Base
     med_compliance && exer_compliance && diet_compliance && other_compliance
   end
 
+  def print_compliance(options={})
+    if Patient.column_names.include?(options[:type])
+      compliant = send(options[:type])
+    else
+      compliant = compliant?
+    end
+    compliant ? 'compliant' : 'noncompliant'
+  end
+
   def name_last_comma_first
     "#{last_name}, #{first_name}"
   end
