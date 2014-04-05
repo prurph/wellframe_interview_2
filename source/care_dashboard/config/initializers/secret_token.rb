c81cb1ccaf3f4d33a7418e429d27c162f695f30e
Prescott Murphy <prurph@gmail.com>
2014-04-05 09:15:33 -0400

    Initial commit: Rails app, add styling/error handling gems, update database.yml

---
+++ source/care_dashboard/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CareDashboard::Application.config.secret_key_base = '3b167b299c5c42c6ef4e8bae09e2903cea511c7b5ea17f6c54d94c17b45820ec3c1b75c46a85b09cd610daa430b2d5514f0c4e2457f1533bf146414cd86d9706'
---
+++ source/care_dashboard/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CareDashboard::Application.config.session_store :cookie_store, key: '_care_dashboard_session'
